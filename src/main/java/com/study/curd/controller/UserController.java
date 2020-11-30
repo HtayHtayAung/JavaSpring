ee9e5eccc3a832f77649cda1d14c258ddb7b6de5
HtayHtayAung <htayhtayaung123@gmail.com>
2020-12-01 00:35:09 +0630

    update

--- src/main/java/com/study/curd/controller/UserController.java
+++ src/main/java/com/study/curd/controller/UserController.java
@@ -1,12 +1,9 @@
 package com.study.curd.controller;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import com.study.curd.model.User;
 import com.study.curd.repository.UserRepo;
 
